db2afd3870a664fada4398cef907d538995105a7
Kclee18 <94232222+Kclee18@users.noreply.github.com>
2023-01-03 15:56:37 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cz3hadrq3044.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "leekarchun-bucket"
+customregion = "us-east-1"
